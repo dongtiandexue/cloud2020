f526417153d640d0cbf5858bdda074723fa1e65e
dongtiandexue <huawei_code@163.com>
2020-05-30 08:02:26 +0800

    订单模块80

---
+++ cloud-consumer-order-80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Running boy
+ * @Description: 订单模块主启动
+ * @date 2020/5/30 7:57
+ */
+@SpringBootApplication
+public class OrderMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
